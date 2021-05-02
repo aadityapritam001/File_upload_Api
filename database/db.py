0aef527f54c0ef30434af33172ab61ce8014c066
Aaditya Raj <37104890+Aadityapritam@users.noreply.github.com>
2021-05-02 22:36:02 +0530

    Update db.py

--- database/db.py
+++ database/db.py
@@ -2,9 +2,9 @@ import pymongo
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 
-clust=MongoClient('mongodb+srv://aaditya:1234567890@cluster0.6qbpm.mongodb.net/resume_data?retryWrites=true&w=majority')
-db=clust['resume_data']
-collection=db['resume_detail']
+clust=MongoClient(<Put your database url here>)
+db=clust[<database name here>]
+collection=db[<collection name here>]
 
 
 def insert(all_file):
